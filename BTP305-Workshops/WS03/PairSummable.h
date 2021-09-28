a353d54c9112bcb05d224a928ffcca1b85f53eba
The-Gary <y.majidin@gmail.com>
2021-09-28 00:04:40 -0400

    intial commit

---
+++ BTP305-Workshops/WS03/PairSummable.h
@@ -0,0 +1,9 @@
+/*
+Name:		Yousef Majidinejad
+Student ID:	101306207
+Email:		ymajidinejad@myseneca.ca
+Section:	NAA
+Date:		September 28, 2021
+I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments
+*/
+
