85f63134ea9e904ebc336e390d6b14af009ad4b7
The-Gary <y.majidin@gmail.com>
2021-09-28 12:20:40 -0400

    part 1 completed

--- BTP305-Workshops/WS03/Pair.h
+++ BTP305-Workshops/WS03/Pair.h
@@ -7,3 +7,62 @@ Date:		September 28, 2021
 I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments
 */
 
+#ifndef SDDS_PAIR_H
+#define SDDS_PAIR_H
+
+#include <iostream>
+
+namespace sdds
+{
+	template<typename V, typename K>
+	class Pair
+	{
+		V m_value{};
+		K m_key{};
+	public:
+		Pair(){}; // default constructor 
+		Pair(const K& key, const V& value);
+		const V& value() const;
+		const K& key() const;
+		void display(std::ostream& os) const;
+	};
+
+	// copies the values referred to by the parameteres into the instance variables
+	template <typename V, typename K>
+	Pair<V, K>::Pair(const K& key, const V& value)
+	{
+		this->m_key = key;
+		this->m_value = value;
+	}
+
+	// returns the value component of the pair
+	template<typename V, typename K>
+	const V& Pair<V, K>::value() const
+	{
+		return this->m_value;
+	}
+
+	// returns the key component of the pair
+	template<typename V, typename K>
+	const K& Pair<V, K>::key() const
+	{
+		return this->m_key;
+	}
+
+	// inserts into stream os the key and the value of the pair
+	template<typename V, typename K>
+	void Pair<V, K>::display(std::ostream& os) const
+	{
+		os << this->m_key << " : " << this->m_value << std::endl;
+	}
+
+	// free helper: calls the member function display() on pair to insert a pair into the os
+	template<typename V, typename K>
+	std::ostream& operator<<(std::ostream& os, const Pair<V, K>& pair)
+	{
+		pair.display(os);
+		return os;
+	}
+
+}
+#endif // !SDDS_PAIR_H
--- BTP305-Workshops/WS03/Set.h
+++ BTP305-Workshops/WS03/Set.h
@@ -7,3 +7,48 @@ Date:		September 28, 2021
 I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments
 */
 
+#ifndef SDDS_SET_H
+#define SDDS_SET_H
+
+namespace sdds
+{
+	template <size_t N, typename T>
+	class Set
+	{
+		T m_element[N]{};
+		size_t m_numOfElements{};
+	public:
+		size_t size() const;
+		const T& get(size_t idx) const;
+		void operator +=(const T& item);
+	};
+
+	// returns the current number of elements in the collection
+	template<size_t N, typename T>
+	size_t Set<N, T>::size() const
+	{
+		return this->m_numOfElements;
+	}
+
+	// returns a reference to the element at the index idx of the statically allocated array (assume the parameter is valid)
+	template<size_t N, typename T>
+	const T& Set<N, T>::get(size_t idx) const
+	{
+		return this->m_element[idx];
+	}
+
+	// if the collection has the capacity for another element, adds a copy of item to the collection, otherwise does nothing
+	template<size_t N, typename T>
+	void Set<N, T>::operator+=(const T& item)
+	{
+		if (this->m_numOfElements < sizeof(this->m_element)/sizeof(this->m_element[0])) {
+			this->m_element[this->m_numOfElements] = item;
+			++this->m_numOfElements;
+		}
+		else {
+			return;
+		}
+	}
+}
+
+#endif // !SDDS_SET_H
