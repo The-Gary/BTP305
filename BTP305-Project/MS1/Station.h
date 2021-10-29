28819eb26e2e7b43944fbe430f7824c8682abf3d
Yousef <y.majidin@gmail.com>
2021-10-29 10:48:50 -0400

    MS1 files added

---
+++ BTP305-Project/MS1/Station.h
@@ -0,0 +1,18 @@
+/*
+Name: Yousef Majidinejad
+Seneca Student ID: 101306207
+Seneca email: ymajidinejad@myseneca.ca
+Date of completion:
+I confirm that I am the only author of this file
+and the content was created entirely by me.
+*/
+
+#ifndef SDDS_STATION_H
+#define SDDS_STATION_H
+
+namespace sdds
+{
+
+}
+
+#endif // !SDDS_STATION_H
---
+++ BTP305-Project/MS1/Utilities.cpp
@@ -0,0 +1,15 @@
+/*
+Name: Yousef Majidinejad
+Seneca Student ID: 101306207
+Seneca email: ymajidinejad@myseneca.ca
+Date of completion:
+I confirm that I am the only author of this file
+and the content was created entirely by me.
+*/
+
+#include "Utilities.h"
+
+namespace sdds
+{
+
+}
