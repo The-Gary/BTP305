88bcdd3b6a0b1a29cc012dd4fe9702fcb71a3314
The-Gary <y.majidin@gmail.com>
2021-11-10 12:56:32 -0500

    updated for part 2

--- BTP305-Workshops/WS07/SongCollection.cpp
+++ BTP305-Workshops/WS07/SongCollection.cpp
@@ -12,7 +12,11 @@ I have done all the coding by myself and only copied the code that my professor 
 #include <fstream>
 #include <iostream>
 #include <iomanip>
+#include <numeric>
 #include <string>
+#include <list>
+#include <iterator>
+#include <cstring>
 
 namespace sdds
 {
@@ -44,19 +48,19 @@ namespace sdds
 			std::getline(file, song);
 			if (file)
 			{
-				newSong.m_sTitle = extract(song, 0, 25);
-				newSong.m_sArtist = extract(song, 25, 25);
-				newSong.m_sAlbum = extract(song, 50, 25);
-				this->trim(newSong.m_sTitle);
-				this->trim(newSong.m_sArtist);
-				this->trim(newSong.m_sAlbum);
-				try { newSong.m_stYear = std::stoi(extract(song, 75, 5)); }
-				catch (...) { newSong.m_stYear = 0; };
-				try { newSong.m_stLength = std::stoi(extract(song, 80, 5)); }
-				catch (...) { newSong.m_stLength = 0; };
-				try { newSong.m_dPrice = std::stod(extract(song, 85, 5)); }
-				catch (...) { newSong.m_dPrice = 0.0; };
-				this->m_vSongs.push_back(newSong);
+				newSong.m_title = extract(song, 0, 25);
+				newSong.m_artist = extract(song, 25, 25);
+				newSong.m_album = extract(song, 50, 25);
+				this->trim(newSong.m_title);
+				this->trim(newSong.m_artist);
+				this->trim(newSong.m_album);
+				try { newSong.m_year = std::stoi(extract(song, 75, 5)); }
+				catch (...) { newSong.m_year = 0; };
+				try { newSong.m_length = std::stoi(extract(song, 80, 5)); }
+				catch (...) { newSong.m_length = 0; };
+				try { newSong.m_price = std::stod(extract(song, 85, 5)); }
+				catch (...) { newSong.m_price = 0.0; };
+				this->m_songs.push_back(newSong);
 			}
 		}
 		file.close();
@@ -64,18 +68,73 @@ namespace sdds
 
 	void SongCollection::display(std::ostream& os)
 	{
-		std::for_each(this->m_vSongs.cbegin(), this->m_vSongs.cend(), [&os](const Song& song) { os << song; });
+		size_t sum{};
+		std::for_each(this->m_songs.cbegin(), this->m_songs.cend(), [&](const Song& song) { os << song << std::endl; sum += song.m_length; });
+		
+		auto hr = sum / 3600;
+		auto min = (sum - hr * 3600) / 60;
+		auto sec = (sum - hr * 3600) % 60;
+
+		auto fill = os.fill();
+		os << "----------------------------------------------------------------------------------------\n";
+		os << "| " << std::setw(77) << std::right << "Total Listening Time: " << hr << ":"
+			<< std::setw(2) << std::setfill('0') << min << ":" << std::setw(2) << sec << " |" << std::endl;;
+		os.fill(fill);
+	}
+
+	void SongCollection::sort(const char* by)
+	{
+		auto compare = [&](const Song& a, const Song& b)
+		{
+			if (strcmp(by, "title") == 0)
+				return a.m_title < b.m_title;
+			if (strcmp(by, "album") == 0)
+				return a.m_album < b.m_album;
+			if (strcmp(by, "length") == 0)
+				return a.m_length < b.m_length;
+			return false;
+		};
+		std::sort(this->m_songs.begin(), this->m_songs.end(), compare);
+	}
+
+	void SongCollection::cleanAlbum()
+	{
+		auto fixAlbum = [](Song& song) -> Song
+		{
+			if (song.m_album == "[None]")
+				song.m_album = "";
+			return song;
+		};
+		std::transform(m_songs.begin(), m_songs.end(), m_songs.begin(), fixAlbum);
+	}
+
+	bool SongCollection::inCollection(const char* artistName)
+	{
+		auto found = [artistName](const Song& song) -> bool
+		{
+			return song.m_artist == artistName;
+		};
+		auto result = std::find_if(m_songs.cbegin(), m_songs.cend(), found);
+		return result != m_songs.cend();
+	}
+
+	std::list<Song> SongCollection::getSongsForArtist(const char* artistName) const
+	{
+		std::list<Song> ret{};
+		std::copy_if(m_songs.begin(), m_songs.end(), std::back_inserter(ret),
+					 [artistName](const Song& song) { return song.m_artist == artistName; });
+		return ret;
 	}
 
 	std::ostream& operator<<(std::ostream& os, const Song& theSong)
 	{
 		auto fill = os.fill();
-		os << "| " << std::setw(20) << std::left << theSong.m_sTitle
-			<< " | " << std::setw(15) << theSong.m_sArtist
-			<< " | " << std::setw(20) << theSong.m_sAlbum
-			<< " | " << std::setw(6) << std::right << ((theSong.m_stYear) ? std::to_string(theSong.m_stYear) : " ")
-			<< " | " << theSong.m_stLength / 60 << ":" << std::setw(2) << std::setfill('0') << theSong.m_stLength % 60
-			<< " | " << theSong.m_dPrice << " |" << std::endl;
+		os << "| " << std::setw(20) << std::left << theSong.m_title
+			<< " | " << std::setw(15) << theSong.m_artist
+			<< " | " << std::setw(20) << theSong.m_album
+			<< " | " << std::setw(6) << std::right << ((theSong.m_year) ? std::to_string(theSong.m_year) : " ")
+			<< " | " << theSong.m_length / 60 << ":" << std::setw(2) << std::setfill('0') << theSong.m_length % 60
+			<< " | " << theSong.m_price << " |";
 		os.fill(fill);
 		return os;
 	}
--- BTP305-Workshops/WS07/SongCollection.h
+++ BTP305-Workshops/WS07/SongCollection.h
@@ -13,27 +13,32 @@ I have done all the coding by myself and only copied the code that my professor 
 #include <string>
 #include <vector>
 #include <iostream>
+#include <list>
 
 namespace sdds
 {
 	struct Song
 	{
-		std::string m_sArtist{};
-		std::string m_sTitle{};
-		std::string m_sAlbum{};
-		double m_dPrice{};
-		size_t m_stYear{};
-		size_t m_stLength{};
+		std::string m_artist{};
+		std::string m_title{};
+		std::string m_album{};
+		double m_price{};
+		size_t m_year{};
+		size_t m_length{};
 	};
 
 	class SongCollection
 	{
-		std::vector<Song> m_vSongs{};
+		std::vector<Song> m_songs{};
 		std::string extract(const std::string& song, size_t begin, size_t range);
 		void trim(std::string& str);
 	public:
 		SongCollection(const char* fileName);
 		void display(std::ostream& os);
+		void sort(const char* by);
+		void cleanAlbum();
+		bool inCollection(const char* artistName);
+		std::list<Song> getSongsForArtist(const char* artistName) const;
 	};
 
 	std::ostream& operator<<(std::ostream& os, const Song& theSong);
